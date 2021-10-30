49387d3a4f28c69d8dcaa2d847483ef7a2a90c8f
Heather Stratton <hastratton@gmail.com>
2021-10-30 11:04:41 -0400

    changed output of images for scripture and added DeskExp

---
+++ DeskExperiment/Models/Desk.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeskExperiment.Models
+{
+    public class Desk
+    {
+        public int ID { get; set; }
+        //private const int MinWidth = 24;
+        //private const int MaxWidth = 96;
+       // private const int MinDepth = 12;
+        //private const int MaxDepth = 48;
+        //1-100: ^([1-9][0-9]?|100)$ or /^([1-9]{1,2}|100)$/
+        //0-100: ^(0|[1-9][0-9]?|100)$
+
+        //private int _width;
+        //private int _depth;
+        [EnumDataType(typeof(DesktopMaterial))]
+        public DesktopMaterial desktopMaterial { get; set; }
+       // public int GetMinWidth() { return MinWidth; }
+        //public int GetMaxWidth() { return MaxWidth; }
+        //public int GetMinDepth() { return MinDepth; }
+       // public int GetMaxDepth() { return MaxDepth; }
+        //[RegularExpression(/^([1 - 9]{1,2}|100)$)]
+        [Range(24, 96)]
+        [Required]
+        public int Width { get; set; }
+        /*{
+            get { return _width; }
+            set
+            {
+                if (ValidatedWidth(value))
+                {
+                    _width = value;
+                }
+            }
+        }*/
+        [Range(12, 48)]
+        [Required]
+        public int Depth { get; set; }
+        /*
+        {
+            get { return _depth; }
+            set
+            {
+                if (ValidatedDepth(value))
+                { _depth = value; }
+            }
+        }*/
+        public int Area
+        {
+            //get { return _width * _depth; }
+            get { return Width * Depth; }
+        }
+       // public bool ValidatedWidth(int widthInput) { if (widthInput >= MinWidth && widthInput <= MaxWidth) return true; else return false; }
+
+        //public bool ValidatedDepth(int depthInput) { if (depthInput >= MinDepth && depthInput <= MaxDepth) return true; else return false; }
+        [Range(0, 7)]
+        [Required]
+
+        public int DrawerCount { get; set; }
+
+
+        /// <summary>
+        /// override of ToString() for testing references
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Format("Width: {0}, Depth: {1}, Material: {2}", Width, Depth, desktopMaterial);
+        }
+
+
+
+
+
+    }/// <summary>
+     /// surface options enum
+     /// </summary>
+    public enum DesktopMaterial
+    {
+        Laminate, Oak, Pine, Rosewood, Veneer
+    }
+}
+
---
+++ DeskExperiment/Models/DeskQuote.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeskExperiment.Models
+{
+    public class DeskQuote
+    {
+       
+
+        private int _rush;
+        private int[,] rushShipping;
+        private const int BaseCost = 200;
+        private const int PerDrawer = 50;
+        private const int OversizeSurface = 1;
+        private const int OversizeLowNum = 1000;
+        private const int OversizeHighNum = 2000;
+
+        public int ID { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string CustomerName { get; set; }
+        public String date { get; set; }
+        // [DataType(DataType.Date)]
+        //public DateTime Date {get;set;}
+        public int RushDays { get; set; }
+        //?[BindProperty]
+        public Desk Desk { get; set; }
+        public int Cost
+        {
+            get
+            {
+                return AreaTotalCost() + CalcDrawerCost() + CalcRushCost() + CalcSurfaceCost();
+            }
+        }
+
+
+        /// <summary>
+        /// w * d of Desk, and calculates cost
+        /// </summary>
+        /// <returns></returns>
+        public int AreaTotalCost()
+        {
+            int oversizedArea = 0;
+            if (Desk.Area > OversizeLowNum)
+            {
+                oversizedArea = Desk.Area - OversizeLowNum;
+            }
+            return BaseCost + (OversizeSurface * oversizedArea);
+
+        }
+        /// <summary>
+        /// calculates total cost of drawers
+        /// </summary>
+        /// <returns></returns>
+        public int CalcDrawerCost()
+        {
+            return PerDrawer * Desk.DrawerCount;
+        }
+        /// <summary>
+        /// calculates total cost of rush order 
+        /// </summary>
+        /// <returns></returns>
+        public int CalcRushCost()
+        {
+            int result = 0;
+            _rush = RushDays;
+            GetRushOrder();
+            int speed;
+            int weight;
+            switch (_rush)
+            {
+                case 3:
+                    speed = 0;
+                    break;
+                case 5:
+                    speed = 1;
+                    break;
+                case 7:
+                    speed = 2;
+                    break;
+                default:
+                    result = 0;
+                    return result;
+            }
+
+            if (Desk.Area < OversizeLowNum) weight = 0;
+            else if (Desk.Area > OversizeLowNum && Desk.Area < OversizeHighNum) weight = 1;
+            else weight = 2;
+
+
+            result = rushShipping[speed, weight];
+            return result;
+        }
+        /// <summary>
+        /// calculates surface cost using enum DesktopMaterial
+        /// </summary> 
+        /// <returns></returns>
+        public int CalcSurfaceCost()
+        {
+            if (Desk.desktopMaterial == DesktopMaterial.Laminate)
+            {
+                return 100;
+            }
+
+            if (Desk.desktopMaterial == DesktopMaterial.Oak)
+            {
+                return 200;
+            }
+
+            if (Desk.desktopMaterial == DesktopMaterial.Pine)
+            {
+                return 50;
+            }
+
+            if (Desk.desktopMaterial == DesktopMaterial.Rosewood)
+            {
+                return 300;
+            }
+
+            if (Desk.desktopMaterial == DesktopMaterial.Veneer)
+            {
+                return 125;
+            }
+
+            return 50;
+
+        }
+        /// <summary>
+        /// override ToString() including custName for reference
+        /// </summary>
+        /// <returns></returns>
+
+        public override string ToString()
+        {
+            return base.ToString() + "\n" +
+               "name:" + CustomerName;
+        }
+
+        /// <summary>
+        /// Opens and Reads the rushorder Price file, then returns it as a 2d Array
+        /// </summary>
+        /// <returns></returns>
+
+        public int[,] GetRushOrder()
+        {
+            try
+            {
+                string[] vs = File.ReadAllLines(@"Resources\rushOrderPrices.txt");
+                int[] rushPrices = Array.ConvertAll(vs, item => int.Parse(item));
+                rushShipping = new int[3, 3];
+                int index = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        rushShipping[i, j] = rushPrices[index];
+                        index++;
+                    }
+                }
+
+            }
+            catch (FileNotFoundException e)
+            {
+                //change catch to something more appropriate
+                //MessageBox.Show(@"File Could Not be Found" + e);
+                
+            }
+
+            return rushShipping;
+        }
+    }
+}
+
