dbebf49ff7d19eda3293bba94168374ea59e8b58
Heather Stratton <hastratton@gmail.com>
2021-11-03 14:49:24 -0400

    to step 6 mvc

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public string Welcome(string name, int ID = 1)//int numTimes = 1)
+        //{
+        //    //return "This is the Welcome action method...";
+        //    //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        //}
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+    }
+}
--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -3,9 +3,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MvcMovie.Data;
+using MvcMovie.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MvcMovie
 {
@@ -13,7 +13,25 @@ namespace MvcMovie
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
