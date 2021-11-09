0192f724b9563f24dab113f37a4b04aaace0f178
Heather Stratton <hastratton@gmail.com>
2021-11-09 12:15:44 -0500

    added uploadPractice

---
+++ UploadPractice/Data/UploadPracticeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UploadPractice.Models;
+
+namespace UploadPractice.Data
+{
+    public class UploadPracticeContext : DbContext
+    {
+        public UploadPracticeContext (DbContextOptions<UploadPracticeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UploadPractice.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ UploadPractice/Models/SeedData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using UploadPractice.Data;
+
+namespace UploadPractice.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new UploadPracticeContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<UploadPracticeContext>>()))
+            {
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Title",
+                        ReleaseDate = DateTime.Parse("2021-11-9"),
+                        Genre = "Genre",
+                        Price = 7.99M,
+                        PhotoPath = "~/images/placeholder.jpg"
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
