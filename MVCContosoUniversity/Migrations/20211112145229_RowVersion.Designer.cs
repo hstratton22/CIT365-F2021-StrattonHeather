aefe4e504d35f199a01e4a0ae07d86ea45c35e5c
Heather Stratton <hastratton@gmail.com>
2021-11-12 10:24:48 -0500

    MVCcontoso tuturial to inheritance

--- MVCContosoUniversity/Migrations/20211112145229_RowVersion.Designer.cs
+++ MVCContosoUniversity/Migrations/20211112145229_RowVersion.Designer.cs
@@ -4,14 +4,16 @@ using MVCContosoUniversity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MVCContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20211112145229_RowVersion")]
+    partial class RowVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
